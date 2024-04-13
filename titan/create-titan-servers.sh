90f6eda352e720ff1f275b009c28150dcbaf91c1
ansike <984315765@qq.com>
2024-04-13 12:18:44 +0800

    feat: 优化脚本

--- titan/create-titan-servers.sh
+++ titan/create-titan-servers.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+# 设置默认哈希值
+default_hash="87E8C67B-EDE3-403A-AB9C-8B268B16FD1D"
+
+# 询问用户输入哈希值
+echo "请输入哈希值（直接回车将使用默认值 '$default_hash'）："
+read -r hash
+
+
+# 如果用户没有输入，则使用默认哈希值
+if [ -z "$hash" ]; then
+  hash="$default_hash"
+fi
+
+
 # 批量部署docker镜像
 for i in $(seq 1 5)
 do
     echo "docker$i"
+    echo "docker run -d --name docker$i -v "/F:/titan/dockerFile/d$i:/root/.titanedge" nezha123/titan-edge"
+    echo "docker exec "docker$i" titan-edge bind --hash="$hash" https://api-test1.container1.titannet.io/api/v2/device/binding"
     docker run -d --name "docker$i" -v "/F:/titan/dockerFile/d$i:/root/.titanedge" nezha123/titan-edge
     # 注意把hash换成自己的
-    docker exec "docker$i" titan-edge bind --hash=87E8C67B-EDE3-403A-AB9C-8B268B16FD1D https://api-test1.container1.titannet.io/api/v2/device/binding
+    docker exec "docker$i" titan-edge bind --hash="$hash" https://api-test1.container1.titannet.io/api/v2/device/binding
 done
 
-# 删除所有的docker镜像
-# docker ps -a|grep nezha123/titan-edge|awk '{print $NF}'|xargs docker rm -f
+echo "大功告成~"
---
+++ titan/delete-titan-servers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "当前nezha123/titan-edge的个数为：$(docker ps -a | grep nezha123/titan-edge | wc -l)"
+
+# 删除所有的 nezha123/titan-edge docker镜像
+docker ps -a | grep nezha123/titan-edge | awk '{print $NF}' | xargs docker rm -f
+
+echo "当前nezha123/titan-edge的个数为：$(docker ps -a | grep nezha123/titan-edge | wc -l)"
+
+echo "大功告成~"
